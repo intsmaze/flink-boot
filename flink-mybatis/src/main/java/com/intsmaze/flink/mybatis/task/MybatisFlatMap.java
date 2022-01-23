dfb0fc48433a8b235335dde4848073f4852e43f5
intsmaze <979222969@qq.com>
2022-01-23 14:53:06 +0800

    mybatis添加异常声明

--- flink-mybatis/src/main/java/com/intsmaze/flink/mybatis/task/MybatisFlatMap.java
+++ flink-mybatis/src/main/java/com/intsmaze/flink/mybatis/task/MybatisFlatMap.java
@@ -35,7 +35,7 @@ public class MybatisFlatMap extends CommonFunction {
      * @date: 2020/10/15 18:33
      */
     @Override
-    public void open(Configuration parameters) {
+    public void open(Configuration parameters) throws Exception {
         super.open(parameters);
         flowService = beanFactory.getBean(FlowServiceImpl.class);
     }
