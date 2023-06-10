0ef5782dbe2f1611e8b6a6b310589269eced2f85
intsmaze <979222969@qq.com>
2023-06-10 13:06:13 +0800

    升級1.16

--- flink-sql/src/main/java/com/intsmaze/flink/sql/XmlUtils.java
+++ flink-sql/src/main/java/com/intsmaze/flink/sql/XmlUtils.java
@@ -2,12 +2,10 @@ package com.intsmaze.flink.sql;
 
 import org.dom4j.Attribute;
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
@@ -56,46 +54,13 @@ public class XmlUtils {
         return map;
     }
 
-    /**
-     * github地址: https://github.com/intsmaze
-     * 博客地址：https://www.cnblogs.com/intsmaze/
-     * 出版书籍《深入理解Flink核心设计与实践原理》
-     *
-     * @auther: intsmaze(刘洋)
-     * @date: 2020/10/15 18:33
-     */
-    public static Map<String, String> readXMLForSql(String pathArr)
-            throws Exception {
-        readFile(new File(pathArr));
-
-        for (int i = 0; i < fileList.size(); i++) {
-            File file1 = fileList.get(i);
-            System.out.println(file1.getName() + "1111111");
-        }
-        Map<String, String> map = new HashMap<String, String>(10);
-        return map;
-    }
 
     private static List<File> fileList = new ArrayList<File>();
 
-    /**
-     * github地址: https://github.com/intsmaze
-     * 博客地址：https://www.cnblogs.com/intsmaze/
-     * 出版书籍《深入理解Flink核心设计与实践原理》
-     *
-     * @auther: intsmaze(刘洋)
-     * @date: 2020/10/15 18:33
-     */
-    private static void readFile(File file) {
-        File[] fs = file.listFiles();
-        for (File f : fs) {
-            if (f.isDirectory()) {
-                readFile(f);
-            }
-            if (f.isFile()) {
-                fileList.add(f);
-            }
-        }
+    public static void main(String[] args) throws Exception {
+        Map<String, String> stringStringMap = XmlUtils.readXMLForSql(new String[]{"etl1.sql"});
+        System.out.println(stringStringMap);
+
     }
 
 }
--- flink-sql/src/main/resources/etl.sql
+++ flink-sql/src/main/resources/etl.sql
@@ -1,15 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <sqls>
 
-  <!-- 查询每个循环根据seqid,testTime 去重下，第一条数据的stepNumber,stepType -->
-	<sql id="sql_temp">
-	  SELECT
-	flowId,
-	cycleNumber,
-	stepNumber,
-	stepType
-FROM
-			sourceData
-	</sql>
+<sql id="source_table">
+CREATE TABLE Orders (
+    order_number BIGINT,
+    price        DECIMAL(32,2),
+    order_time   TIMESTAMP(3)
+) WITH (
+  'connector' = 'datagen'
+)
+</sql>
+
+<sql id="sink_table">
+	  CREATE TABLE print_sink (
+    order_number BIGINT,
+     max_price  DECIMAL(32,2)
+    ) WITH (
+    'connector' = 'print'
+    )
+</sql>
+
+
+<sql id="agg_view">
+create view agg_view as
+select order_number,max(price) from Orders group by order_number
+</sql>
+
+<sql id="insert_table">
+insert into print_sink
+select * from agg_view
+</sql>
 
 </sqls>
